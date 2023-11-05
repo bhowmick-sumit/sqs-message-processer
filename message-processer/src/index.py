70bfab07ee871cad9e6c9bb0b35b73b019c9716c
sumit-kumar-bhowmick <sumit.k.bhowmick@gmail.com>
2023-11-05 16:40:38 +0530

    added message body

--- message-processer/src/index.py
+++ message-processer/src/index.py
@@ -1,10 +1,26 @@
 from aws_lambda_powertools import Logger
+import os
+import json
+import boto3
+from datetime import datetime
 
 
 log = Logger("handler")
+s3_resource = boto3.resource("s3")
+BUCKET_NAME = os.getenv("DESTINATION_BUCKET_NAME") 
 
 
 def handler(event, context):
-    
-    for message in event["Records"]:
-        log.info(f"the message is {message}")
+    for record in event["Records"]:
+        message = record["body"]
+        fileName = json.loads(message).get("fileName")
+        if fileName != None:
+            log.info(f"the message is {message}")
+            # s3_resource.Bucket(BUCKET_NAME).put_object(Key=fileName,Body=message)
+
+        else:
+            raise "fileName not found"
+        
+
+
+handler(event={"Records":{"body":{},}},context={})
